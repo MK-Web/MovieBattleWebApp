83fd5f661ca834e98e70b9f6e9651296665b3237
MK-Web <mk010web@gmail.com>
2021-01-09 20:13:59 +0100

    first commit

---
+++ utils.js
@@ -0,0 +1,12 @@
+//Helper Function
+const debounce = (func,delay) => {
+    let timeoutId;
+    return (...args) => {
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+        timeoutId = setTimeout(() => {
+            func.apply(null,args);
+        },delay);
+    };
+};
